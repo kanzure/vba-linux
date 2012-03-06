1666c7afd1e102e723de8f8451888259f80812ec
Robert McIntyre <rlm@mit.edu>
2012-03-06 14:27:20 -0600

    remove useless shell scripts

--- play-movie.sh
+++
@@ -1,3 +0,0 @@
-/home/r/proj/vba-restore/build/artifacts/bin/VisualBoyAdvance \
-    --playmovie=/home/r/proj/vba-restore/build/movie.vba \
-    /home/r/proj/pokemon-escape/roms/yellow.gbc         
--- wrong.sh
+++
@@ -1,8 +0,0 @@
-cd ./src/gba
-
-g++ -DHAVE_CONFIG_H \
-    -I. -I../../../src/gba -I../..  -I../../../src \
-    -DSDL -DSYSCONFDIR=\"/usr/local/etc\"  \
-    -fno-exceptions  -g -O2 \
-    -MT remote.o -MD -MP -MF .deps/remote.Tpo -c \
-    -o remote.o ../../../src/gba/remote.cpp
